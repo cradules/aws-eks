9da35523966b32997464561fa8f238d23c70c1d6
Radulescu Constantin <seaman105@yahoo.com>
2022-03-08 00:30:00 +0200

    Remove depends

--- main.tf
+++ main.tf
@@ -106,7 +106,6 @@ module "karpenter_irsa" {
 
 # Install karpenter
 module "karpenter" {
-  depends_on = [module.eks]
   source     = "terraform-module/release/helm"
   namespace  = "karpenter"
   repository = "https://charts.karpenter.sh/"
