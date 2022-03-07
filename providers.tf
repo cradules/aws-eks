f20ec84f1409ff2f289117c6e2aa31b28304046f
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 20:21:41 +0200

    fmt

--- providers.tf
+++ providers.tf
@@ -18,8 +18,8 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    host = module.eks.cluster_id
+    host               = module.eks.cluster_id
     client_certificate = module.eks.cluster_certificate_authority_data
-    token = data.aws_eks_cluster_auth.eks-auth.token
+    token              = data.aws_eks_cluster_auth.eks-auth.token
   }
 }
