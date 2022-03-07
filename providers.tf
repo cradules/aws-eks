2353de3d4d268f1056d93ffe6b2bd6eef0524e14
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 21:26:35 +0200

    Try with exec

--- providers.tf
+++ providers.tf
@@ -21,10 +21,22 @@ provider "kubernetes" {
   client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
   token              = data.aws_eks_cluster_auth.eks-auth.token
 }
+#provider "helm" {
+#  kubernetes {
+#    host               = data.aws_eks_cluster.eks-cluster.endpoint
+#    client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+#    token              = data.aws_eks_cluster_auth.eks-auth.token
+#  }
+#}
+
 provider "helm" {
   kubernetes {
-    host               = data.aws_eks_cluster.eks-cluster.endpoint
-    client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-    token              = data.aws_eks_cluster_auth.eks-auth.token
+    host                   = data.aws_eks_cluster.eks-cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.eks-cluster-name]
+      command     = "aws"
+    }
   }
 }
