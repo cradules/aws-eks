42a9bcc95d13b343768f6eec3bb9ddcac10d803a
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 15:27:37 +0200

    Fix provider

--- providers.tf
+++ providers.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.26.0"
     }
   }
-}
+  required_version = ">= 1.1.0"
 
+  cloud {
+    organization = "guts"
+
+    workspaces {
+      name = "guts-eks-dev"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-2"
 }
