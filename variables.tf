43f2c81fb8c0d748e2fe3949e62a8e2373a44860
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 14:47:31 +0200

    Fix

--- variables.tf
+++ variables.tf
@@ -3,38 +3,38 @@
 variable "eks-cluster-name" {
   description = "EKS Cluster Name"
   type        = string
-  default = "eks-dev"
+  default     = "eks-dev"
 }
 
 variable "eks-cluster-version" {
   description = "EKS Cluster Version"
   type        = string
-  default = "1.21"
+  default     = "1.21"
 }
 
 #VPC VARS
 variable "vpc-name" {
   description = "AWS VPC Name"
   type        = string
-  default = "vpc-dev"
+  default     = "vpc-dev"
 }
 
 variable "vpc-cidr" {
   description = "AWS VPC CIRDs"
   type        = string
-  default = "192.168.0.0/16"
+  default     = "192.168.0.0/16"
 }
 
 variable "vpc-azs" {
   description = "VPC Availability Zones"
   type        = list(string)
-  default = ["us-est-2a, us-east-2b, us-east-2c"]
+  default     = ["us-est-2a", "us-east-2b", "us-east-2c"]
 }
 
 variable "vpc-public-subnets" {
   description = "VPC public subnets"
   type        = list(string)
-  default = ["192.168.0.0/18", "192.168.64.0/18", "192.168.128.0/18"]
+  default     = ["192.168.0.0/18", "192.168.64.0/18", "192.168.128.0/18"]
 }
 
 variable "vpc-enable-dns-hostnames" {
@@ -47,5 +47,5 @@ variable "vpc-enable-dns-hostnames" {
 variable "environment" {
   description = "VPC Environment Tag"
   type        = string
-  default = "dev"
+  default     = "dev"
 }
