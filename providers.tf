2870c75ce7f88139f4f187061ed0f5b47d741428
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 15:30:20 +0200

    Fix fmt

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
-  required_version = ">= 1.1.0"
-
   cloud {
     organization = "guts"
 
