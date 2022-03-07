c2d509cbec6308f52ac9f03b9b662bd5c205e0ce
Constantin Radulescu <seaman105@yahoo.com>
2022-03-07 21:21:43 +0200

    Fix index

--- providers.tf
+++ providers.tf
@@ -18,13 +18,13 @@ provider "aws" {
 
 provider "kubernetes" {
   host               = data.aws_eks_cluster.eks-cluster.endpoint
-  client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
+  client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
   token              = data.aws_eks_cluster_auth.eks-auth.token
 }
 provider "helm" {
   kubernetes {
     host               = data.aws_eks_cluster.eks-cluster.endpoint
-    client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
+    client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
     token              = data.aws_eks_cluster_auth.eks-auth.token
   }
 }
