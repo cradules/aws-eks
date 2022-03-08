0f304b737bff1e52bb6f569fedc3bf25449162b3
Radulescu Constantin <seaman105@yahoo.com>
2022-03-08 23:29:35 +0200

    Try to fix:Error: Kubernetes cluster unreachable: invalid configuration: no configuration has been provided, try setting KUBERNETES_MASTER environment variable

--- providers.tf
+++ providers.tf
@@ -27,8 +27,12 @@ provider "kubernetes" {
   load_config_file   = false
   host               = data.aws_eks_cluster.eks-cluster.endpoint
   client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  token              = data.aws_eks_cluster_auth.eks-auth.token
   insecure           = true
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
 }
 
 provider "helm" {
