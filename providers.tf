3e423d6265d85fe2f8fefdd06e1c0dd4c240e6fa
Radulescu Constantin <seaman105@yahoo.com>
2022-03-08 13:56:23 +0200

    Downgrade kubernetes provider to 1.10

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "1.11.1"
+      version = "1.10.0"
     }
   }
   cloud {
@@ -27,11 +27,8 @@ provider "kubernetes" {
   load_config_file   = false
   host               = data.aws_eks_cluster.eks-cluster.endpoint
   client_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", var.eks-cluster-name]
-    command     = "aws"
-  }
+  token = data.aws_eks_cluster_auth.eks-auth.token
+  alias = "override"
 }
 
 provider "helm" {
