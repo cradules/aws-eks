86e298b1fea82e5db48c7ad8db57772dd5ed6b75
Radulescu Constantin <seaman105@yahoo.com>
2022-03-09 02:47:20 +0200

    [Fix](karpenter): controller.provisioning Could not launch node, launching instances, with fleet error(s), UnauthorizedOperation: You are not authorized to perform this operation

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ module "karpenter_irsa" {
 
 resource "aws_iam_instance_profile" "karpenter" {
   name = "KarpenterNodeInstanceProfile-${var.eks-cluster-name}"
-  role = module.eks.cluster_iam_role_name
+  role = module.eks.eks_managed_node_groups["default"].iam_role_arn
 }
 
 #Install karpenter
